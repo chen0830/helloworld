488cce3410ed30577b87c363633b897c3558ea71
Administrator <Administrator@PC652>
2019-08-26 08:39:48 +0800

    test1.2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!!! --chen");
-		int chen = 20;
+		String chen = "hahahah";
+		System.out.println(chen);
 	}
 
 }
